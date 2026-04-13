ccb4d7a7e96c1e20998656b3bacc3b62cef6037c
TheTripode <thetripode06@gmail.com>
2026-04-13 23:18:59 +0200

    login hecho (no probado) y security.yaml modificado

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'ctrl_login')]
+    public function login(AuthenticationUtils $authUtils)
+    {
+        // obtener último email ingresado
+        $lastEmail = $authUtils->getLastUsername();
+
+        // obtener error de login si lo hay
+        $error = $authUtils->getLastAuthenticationError();
+
+        return $this->render('login.html.twig', [
+            'last_email' => $lastEmail,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'ctrl_logout')]
+    public function logout()
+    {
+    }
+}
