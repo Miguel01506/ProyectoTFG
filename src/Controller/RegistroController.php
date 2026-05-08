2fe896858b166db63db2aa959ae8ed50359e61a5
Miguel01506 <miguelpastor006@gmail.com>
2026-05-08 21:16:01 +0200

    foto default usu nuevo

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -78,6 +78,7 @@ class RegistroController extends AbstractController
         $nuevoUsu->setEmail($email);
         $nuevoUsu->setClave(password_hash($pass, PASSWORD_DEFAULT));
         $nuevoUsu->setNombreUsuario($username);
+        $nuevoUsu->setFotoPerfil("default.png");
         $nuevoUsu->setRol('usuario');
         $nuevoUsu->setToken(bin2hex(random_bytes(32)));
         $nuevoUsu->setActivo(false);
